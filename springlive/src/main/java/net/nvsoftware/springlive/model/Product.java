913f5299db5967ba96f8c06ca4ef0016d5a4dc45
Kyle Deng <kyle3228d@gmail.com>
2023-10-09 15:30:52 -0700

    remove JsonIgnore

--- springlive/src/main/java/net/nvsoftware/springlive/model/Product.java
+++ springlive/src/main/java/net/nvsoftware/springlive/model/Product.java
@@ -3,9 +3,9 @@ package net.nvsoftware.springlive.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties({"title", "description"})
+//@JsonIgnoreProperties({"title", "description"})
 public class Product {
-
+    //@JsonIgnore
     private String productId;
     private String title;
     private String description;
