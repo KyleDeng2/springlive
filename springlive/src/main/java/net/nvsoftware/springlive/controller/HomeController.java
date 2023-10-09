b8c194dc0a9ed2f2e81a2c77d9a82bc04d7a9504
Kyle Deng <kyle3228d@gmail.com>
2023-10-08 21:16:38 -0700

    add User Model, refactor to restcontroller, set up postman to test api

--- springlive/src/main/java/net/nvsoftware/springlive/controller/HomeController.java
+++ springlive/src/main/java/net/nvsoftware/springlive/controller/HomeController.java
@@ -1,15 +1,26 @@
 package net.nvsoftware.springlive.controller;
 
+import net.nvsoftware.springlive.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "hi";
     }
+
+    @RequestMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("36090");
+        user.setName("Ken");
+        user.setEmail("Ken@gmail.com");
+        return user;
+    }
+
 }
