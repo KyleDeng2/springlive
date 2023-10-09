32d3beb455ea39310a2aa60d5de36b3e9854f73a
Kyle Deng <kyle3228d@gmail.com>
2023-10-08 23:02:57 -0700

    Add Product CRUD,save,getALL getById,deletedById

--- springlive/src/main/java/net/nvsoftware/springlive/service/ProductService.java
+++ springlive/src/main/java/net/nvsoftware/springlive/service/ProductService.java
@@ -2,6 +2,13 @@ package net.nvsoftware.springlive.service;
 
 import net.nvsoftware.springlive.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
     Product save(Product product);
+
+    List<Product> getAll();
+    Product getById(String id);
+
+    String deleteById(String id);
 }
