de1b4de1fa0438b6123d3e4be568c8489d425334
Kyle Deng <kyle3228d@gmail.com>
2023-10-10 15:03:59 -0700

    For ProductService CRUD, getById

--- ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductService.java
+++ ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductService.java
@@ -2,7 +2,10 @@ package net.nvsoftware.ProductService.service;
 
 
 import net.nvsoftware.ProductService.model.ProductRequest;
+import net.nvsoftware.ProductService.model.ProductResponse;
 
 public interface ProductService {
     long addProduct(ProductRequest productRequest);
+
+    ProductResponse getById(long id);
 }
