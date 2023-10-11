ac46cddf6e1e6e4b551d80e463730ce5f24154e4
Kyle Deng <kyle3228d@gmail.com>
2023-10-10 17:07:48 -0700

    add local log for OrderService controller and service layer

--- OrderService/src/main/java/net/nvsoftware/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/controller/OrderController.java
@@ -1,5 +1,6 @@
 package net.nvsoftware.OrderService.controller;
 
+import lombok.extern.log4j.Log4j2;
 import net.nvsoftware.OrderService.model.OrderRequest;
 import net.nvsoftware.OrderService.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +15,16 @@ import javax.ws.rs.core.Response;
 
 @RestController
 @RequestMapping("/order")
+@Log4j2
 public class OrderController {
     @Autowired
     private OrderService orderService;
 
     @PostMapping("/placeOrder")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Start: OrderService Controller placeOrder");
         long orderId = orderService.placeOrder(orderRequest);
+        log.info("End: OrderService Controller placeOrder");
         return new ResponseEntity<>(orderId, HttpStatus.OK);
     }
 }
