893f071f5d608dd05297289319714fc4a293dde3
Kyle Deng <kyle3228d@gmail.com>
2023-10-11 18:17:08 -0700

    For OrderService Feign Call doPayment

--- OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
@@ -1,9 +1,11 @@
 package net.nvsoftware.OrderService.service;
 
 import lombok.extern.log4j.Log4j2;
+import net.nvsoftware.OrderService.client.PaymentServiceFeignClient;
 import net.nvsoftware.OrderService.client.ProductServiceFeignClient;
 import net.nvsoftware.OrderService.entity.OrderEntity;
 import net.nvsoftware.OrderService.model.OrderRequest;
+import net.nvsoftware.OrderService.model.PaymentRequest;
 import net.nvsoftware.OrderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,8 +20,11 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private ProductServiceFeignClient productServiceFeignClient;
 
+    @Autowired
+    private PaymentServiceFeignClient paymentServiceFeignClient;
+
     @Override
-    public long placeOrder(OrderRequest orderRequest) {
+    public long placeOrder(OrderRequest orderRequest) { //TODO: transactions
         log.info("Start: OrderService placeOrder");
         // use OrderService to create OrderEntity with status CREATED, ORM JPA save to database
         OrderEntity orderEntity = OrderEntity.builder()
@@ -36,8 +41,28 @@ public class OrderServiceImpl implements OrderService {
         productServiceFeignClient.reduceQuantity(orderEntity.getProductId(), orderEntity.getQuantity());
         log.info("Process: OrderService placeOrder FeignCall ProductService reduceQuantity");
 
+        PaymentRequest paymentRequest = PaymentRequest.builder()
+                .orderId(orderEntity.getId())
+                .paymentMode(orderRequest.getPaymentMode())
+                .totalAmount(orderRequest.getTotalAmount())
+                .build();
+
+        String orderStatus = null;
+
+        try {
+            paymentServiceFeignClient.doPayment(paymentRequest);
+            orderStatus = "PAID";
+            log.info("Process: OrderService placeOrder FeignCall PaymentService doPayment PAID");
+        } catch (Exception e) {
+            orderStatus = "PAYMENT_FALIDED";
+            log.info("Process: OrderService placeOrder FeignCall PaymentService doPayment PAID");
+        }
+
+        orderEntity.setOrderStatus(orderStatus);
+        orderRepository.save(orderEntity);
+
         // call PaymentService to charge, if success, mark order PAID, else CANCELLED
-        log.info("End: OrderService placeOrder");
+        log.info("End: OrderService placeOrder Done with orderId: " + orderEntity.getId());
         return orderEntity.getId();
     }
 }
