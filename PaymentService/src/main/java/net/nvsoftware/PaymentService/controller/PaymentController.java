d16b648a4c6a500a4afe941aeb012a24f724a630
Kyle Deng <kyle3228d@gmail.com>
2023-10-11 17:47:11 -0700

    For PaymentService, doPayment

--- PaymentService/src/main/java/net/nvsoftware/PaymentService/controller/PaymentController.java
+++ PaymentService/src/main/java/net/nvsoftware/PaymentService/controller/PaymentController.java
@@ -1,2 +1,22 @@
-package net.nvsoftware.PaymentService.controller;public class PaymentController {
+package net.nvsoftware.PaymentService.controller;
+
+import net.nvsoftware.PaymentService.model.PaymentRequest;
+import net.nvsoftware.PaymentService.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.core.Response;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+    @PostMapping("/doPayment")
+    public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
+        long paymentId = paymentService.doPayment(paymentRequest);
+        return new ResponseEntity<>(paymentId, HttpStatus.OK);
+    }
 }
--- PaymentService/src/main/java/net/nvsoftware/PaymentService/repository/PaymentRepository.java
+++ PaymentService/src/main/java/net/nvsoftware/PaymentService/repository/PaymentRepository.java
@@ -1,2 +1,7 @@
-package net.nvsoftware.PaymentService.repository;public interface PaymentRepository {
+package net.nvsoftware.PaymentService.repository;
+
+import net.nvsoftware.PaymentService.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<PaymentEntity,Long> {
 }
