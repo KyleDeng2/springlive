6cb5144b125dd1c0fee6c871960e6eb4a82f5470
Kyle Deng <kyle3228d@gmail.com>
2023-10-14 18:23:19 -0700

    intro Spring Cloud LoadBalanced to make restTemplate use serviceName not ip+port

--- OrderService/src/main/java/net/nvsoftware/OrderService/OrderServiceApplication.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package net.nvsoftware.OrderService;
 import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +17,7 @@ public class OrderServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
